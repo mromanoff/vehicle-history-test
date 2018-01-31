5dd1af1ac050551c10d0221de0b20de99b5bfdee
Michael Romanoff <zelcie@gmail.com>
2018-01-30 18:43:28 -0800

    clean up

--- src/Gallery/CategoryPicker.js
+++ src/Gallery/CategoryPicker.js
@@ -12,6 +12,7 @@ const Button = styled.button`
   color: ${props => props.theme.color.white};
   transition: background-color 100ms ease-out 100ms;
   padding: ${props => props.theme.size.xs} ${props => props.theme.size.sm};
+  border: 1px solid ${props => props.theme.color.white};
 
   &:hover,
   &.active {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { injectGlobal } from 'styled-components';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// Global style
-injectGlobal`
-  body {
-    background-color: white;
-    font-family: sans-serif;
-  }
-`;
